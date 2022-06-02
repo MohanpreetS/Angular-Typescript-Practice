c2aab8bd483c186b78c74c21539b3ab3fdb56919
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-02 13:09:04 -0400

    ts lesson 3 - inferred types

---
+++ typescript/lesson 3/sandbox.js
@@ -0,0 +1,10 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
---
+++ typescript/lesson 3/sandbox.ts
@@ -0,0 +1,13 @@
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// character = 20;
+character = 'luigi';
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
