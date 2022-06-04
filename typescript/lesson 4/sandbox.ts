4fcb8f56058f2606169a09024a30ce0f157f6d77
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-04 13:09:04 -0400

    ts lesson 3 - inferred array types

---
+++ typescript/lesson 4/sandbox.ts
@@ -0,0 +1,12 @@
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+
+var numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
