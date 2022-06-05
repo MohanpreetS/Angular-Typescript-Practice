a3467d0d2774fa80318fa02c66d5e120aa036aaf
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-05 13:09:04 -0400

    ts lesson 3 - mixed arrays and objects

--- typescript/lesson 4/sandbox.js
+++ typescript/lesson 4/sandbox.js
@@ -7,3 +7,15 @@ var numbers = [10, 20, 12, 15];
 numbers.push(25);
 // numbers.push('shaun');
 // numbers[0] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
--- typescript/lesson 4/sandbox.ts
+++ typescript/lesson 4/sandbox.ts
@@ -10,3 +10,16 @@ var numbers = [10, 20, 12, 15];
 numbers.push(25);
 // numbers.push('shaun');
 // numbers[0] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
