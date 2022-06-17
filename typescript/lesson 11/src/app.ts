01182cb16dae5a2c913faf8e272701e72ae3fb1a
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-17 13:09:04 -0400

    typecasting practice

--- typescript/lesson 11/src/app.ts
+++ typescript/lesson 11/src/app.ts
@@ -8,3 +8,19 @@ console.log(anchor.href);
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
