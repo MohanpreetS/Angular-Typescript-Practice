a5a7f9533149291808e03b5a39280b1fb97d185f
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-20 13:09:04 -0400

    defining classes

---
+++ typescript/lesson 12/public/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
