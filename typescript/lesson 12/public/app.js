9702dc8ba8201c3d04bfd0f13fc7a5de27250b43
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-20 13:09:04 -0400

    class instances and manipulation

--- typescript/lesson 12/public/app.js
+++ typescript/lesson 12/public/app.js
@@ -11,3 +11,10 @@ var Invoice = /** @class */ (function () {
     };
     return Invoice;
 }());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+console.log(invoices);
