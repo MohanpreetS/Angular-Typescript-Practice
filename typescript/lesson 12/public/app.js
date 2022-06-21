876728543d9949f1f6d5f3987440c46f071da4b5
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-21 13:09:04 -0400

    dom in classes

--- typescript/lesson 12/public/app.js
+++ typescript/lesson 12/public/app.js
@@ -18,3 +18,14 @@ invoices.push(invOne);
 invoices.push(invTwo);
 // invoices.push({ name: 'shaun' });
 console.log(invoices);
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
