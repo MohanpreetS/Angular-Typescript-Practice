efc0c373e80a7561d279ac30453ad2b7f1b5dc6b
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-06-26 13:09:04 -0400

    interface intro

---
+++ typescript/lesson 15/public/app.js
@@ -0,0 +1,11 @@
+const me = {
+    name: 'shaun',
+    //age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    },
+};
---
+++ typescript/lesson 15/src/app.ts
@@ -0,0 +1,22 @@
+import { Invoice } from './classes/Invoice.js';
+
+// interfaces
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
+
